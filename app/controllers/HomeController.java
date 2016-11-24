4cae7625a35dcf0d8fa292a5e9c16f65c1725d38
saul sm <saul.salazar.mendez@gmail.com>
2016-11-23 23:17:44 -0600

    se agrego crear persona y ver datos

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,8 +1,25 @@
 package controllers;
 
-import play.mvc.*;
 
 import views.html.*;
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.Transaction;
+import play.mvc.*;
+import play.data.*;
+import play.db.ebean.*;
+import com.avaje.ebean.Model;
+import static play.data.Form.*;
+import static play.libs.Json.toJson;
+
+
+
+import models.*;
+import javax.inject.Inject;
+import javax.persistence.PersistenceException;
+import java.util.List;
+
+
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -18,6 +35,23 @@ public class HomeController extends Controller {
      */
     public Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+
+
+    public Result verPersonas(){
+      List<Persona> personas = new Model.Finder(String.class, Persona.class).all();
+      return ok(views.html.verPersonas.render(personas));
+    }
+
+    public Result crear(){
+      return ok(views.html.nuevo.render("Nuevo nombre"));
+    }
+
+    public Result guardarPersona(){
+      //cargamos los datos del formulario
+      Persona persona = Form.form(Persona.class).bindFromRequest().get();
+      persona.save();
+      return redirect(routes.HomeController.verPersonas());
     }
 
 }
