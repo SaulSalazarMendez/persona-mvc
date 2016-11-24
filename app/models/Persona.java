3f05757a3ed10f88d71403121ab6afb7a70ccc24
saul sm <saul.salazar.mendez@gmail.com>
2016-11-23 22:45:53 -0600

    se modifico el modelo de datos
    se añadieron los identificadores de entidad e id para la base de datos

--- app/models/Persona.java
+++ app/models/Persona.java
@@ -1,12 +1,22 @@
 package models;
 
+
+import java.util.*;
+import javax.persistence.*;
+
 import com.avaje.ebean.Model;
 import play.db.ebean.*;
 import play.data.validation.*;
 
-public class Persona extends Model{
 
+@Entity
+public class Persona extends Model{
+  @Id
   public Long id;
 
   public String nombre;
+ /**
+ * para buscar elementos
+ */
+  public static Finder<Long, Persona> find = new Finder<Long,Persona>(Persona.class);
 }
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,18 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table persona (
+  id                            bigint not null,
+  nombre                        varchar(255),
+  constraint pk_persona primary key (id)
+);
+create sequence persona_seq;
+
+
+# --- !Downs
+
+drop table if exists persona;
+drop sequence if exists persona_seq;
+
